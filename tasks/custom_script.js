288014baf730e9e0c5be17a6930a49747ad33713
Chandanapagudala <120040608+Chandanapagudala@users.noreply.github.com>
2025-04-06 00:16:36 -0500

    Create custom_script.js

---
+++ tasks/custom_script.js
@@ -0,0 +1,15 @@
+let x=5;
+let y=7;
+console.log(x);
+console.log(y);
+
+let a=x+9;
+let b=y-6;
+let c=y/x;
+let d=y*x;
+let e=x%y;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
