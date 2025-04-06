fbbe0a3e0c991335bc1383f4a0269e7d2f3e9c6f
Chandanapagudala <120040608+Chandanapagudala@users.noreply.github.com>
2025-04-06 00:18:59 -0500

    Create custom_script1.js

---
+++ tasks/custom_script1.js
@@ -0,0 +1,5 @@
+function convertToUpper() {
+var inputText = document.getElementById("inputField").value;
+var upperCaseText = inputText.toUpperCase();
+document.getElementById("outputField").value = upperCaseText;
+}
